b15bf1a85044e1a82e7ffd1e70a75fb3c465b362
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-15 08:15:57 -0500

    log stuff.

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -22,4 +22,17 @@ KAPI void log_output(log_level level, const char* message, ...) {
     //!  DON'T DO THAT!!!
     char out_message[32000];
     memset(out_message, 0, sizeof(out_message));
+
+    // format original message
+    // NOTE: oddly enough, MS's headers override the GCC/Clang va_list type with a "tytpedef_char* va_list" in some cases,
+    //  and as a result throws a astrange error here.  The workaround for now is to just use __builtin_va_list, which is the type GCC/Clang's va_start expects.
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, message);
+    vsnprintf(out_message, 32000, message, arg_ptr);
+    va_end(arg_ptr);
+
+    sprintf(out_message, "%s%s\n", level_strings[level], out_message);
+
+    // TODO: Platform specific output
+    printf("%s", out_message);
 }
